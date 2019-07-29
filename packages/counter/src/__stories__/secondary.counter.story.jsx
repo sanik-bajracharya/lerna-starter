b2c5413c2972e53021650d76818f2def7b4db1bd
Sanik Bajracharya <sanik.bajracharya@gstaples.com>
2019-07-29 16:11:12 -0600

    fix: counter fix issue

--- packages/counter/src/__stories__/secondary.counter.story.jsx
+++ packages/counter/src/__stories__/secondary.counter.story.jsx
@@ -5,7 +5,7 @@ import Counter from '..';
 
 const stories = storiesOf('Counter', module);
 
-stories.add('Secondary counter Example', () => (
+stories.add('Secondary counter Example - Fix1', () => (
   <Counter>
     {({ increase, decrease, count }) => (
       <div>
